25a1a8808723a156928bf21c8d387fa12937c7e4
Teslak <theteslak@gmail.com>
2026-05-30 13:36:59 -0700

    og

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Default fallback snippet to satisfy search engine indexing requirements and web app manifest description constraints.
-	description: "Evil Army — хроники, статьи, заметки и архив нашей истории с 2018 года",
+	description: "Хроники лампового комьюнити, статьи и заметки",
 	// Global document language identifier to assist web browsers, crawlers, and accessibility tools in text synthesis.
 	lang: "ru-RU",
 	// Social media meta tag property used by crawlers (e.g., Open Graph) to deliver properly localized preview cards.
